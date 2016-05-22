8fa65ced0b10ef304fd3bfc82e7b6c2e7e0848e3
Idris Raja <idris.raja@gmail.com>
2016-05-22 12:24:02 -0500

    add core data managed object

---
+++ nasaPOD/model/Photo.swift
@@ -0,0 +1,17 @@
+//
+//  Photo.swift
+//  nasaPOD
+//
+//  Created by id on 5/22/16.
+//  Copyright © 2016 id. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Photo: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
