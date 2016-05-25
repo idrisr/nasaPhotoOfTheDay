5ede52dd6bdcaa50bdeaa34e8e3a55037258b347
Idris Raja <idris.raja@gmail.com>
2016-05-25 18:43:45 -0500

    show webview for youtube player if media type
    is video

--- DetailViewController.swift
+++ DetailViewController.swift
@@ -13,6 +13,7 @@ class DetailViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var webView: UIWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,5 +21,28 @@ class DetailViewController: UIViewController {
         self.imageView.contentMode = .ScaleAspectFit
         self.textView.text = photo?.explanation
         self.title = photo?.title
+
+        switch photo!.media_type! {
+            case .video:
+                let url = NSURL(string: photo!.url!)
+                let request = NSURLRequest(URL: url!)
+                self.webView.loadRequest(request)
+                self.webView.allowsInlineMediaPlayback = true
+                self.imageView.hidden = true
+
+            case .image:
+                self.imageView.image = photo?.image
+                self.imageView.contentMode = .ScaleAspectFit
+                self.webView.hidden = true
+        }
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(true)
+    }
+}
+
+extension DetailViewController: UIWebViewDelegate {
+    func webViewDidFinishLoad(webView: UIWebView) {
     }
 }
